75d31d1840d8788c0e53744b1b24aef9ec03f206
UltimaChaos <74316923+RedMatrix1@users.noreply.github.com>
2023-10-13 12:00:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,9 @@ import subprocess
 
 commands = {
     "send message": "send_message.py",
-    "get message": "get_message.py",
-    "add reaction": "reactions.py",
-    "join server": "join_server.py",
     "leave server": "leave_server.py",
-    "create guild": "create_guild.py",
-    "change about me": "change_about.py",
-    "edit message": "edit_message.py",
-    "upload file": "file_upload.py",
     "get channels": "get_channels.py",
     "get servers": "get_servers.py",
-    "get dm": "get_dm.py"
 }
 
 root_directory = os.getcwd()
